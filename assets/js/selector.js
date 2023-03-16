e2c51b1b55dfdc01038a90ddf78538c977ea9ce0
Ryan Evans <rdevans87@gmail.com>
2023-03-16 08:22:08 -0400

    Added favicon to sight, and refactors code to display the story template dropdown only when screenplay is selected

--- assets/js/selector.js
+++ assets/js/selector.js
@@ -1,33 +1,60 @@
+// Function to select a story and display it on the page
 function selectStory() {
-    // Get the selected story type and screenplay format (if applicable)
-    const storyType = document.getElementById('storyType').value;
-    const screenplayFormat = document.getElementById('screenplayFormat')?.value;
+  // Get the selected story type and screenplay format (if applicable)
+  const storyType = document.getElementById('storyType').value;
+  const screenplayFormat = document.getElementById('screenplayFormat')?.value;
 
-    // Check if a story type has been selected
-    if (!storyType) {
-      alert('Please select a story type');
-      return;
-    }
-
-    // Generate the selected story text
-    let selectedStory = '';
-    if (storyType === 'Screenplay' && screenplayFormat) {
-      selectedStory = `Selected Story: ${screenplayFormat} ${storyType}`;
-    } else {
-      selectedStory = `Selected Story: ${storyType}`;
-    }
-
-    // Display the selected story on the page
-    document.getElementById('selectedStory').innerHTML = `<h2>${selectedStory}</h2>`;
+  // Check if a story type has been selected
+  if (!storyType) {
+    alert('Please select a story type');
+    return;
   }
 
-  // Show the screenplay formats dropdown if the Screenplay option is selected
-  document.getElementById('storyType').addEventListener('change', (event) => {
-    const screenplayFormatsDiv = document.querySelector('.screenplay-formats');
-    if (event.target.value === 'Screenplay')
+  // Generate the selected story text
+  let selectedStory = '';
+  if (storyType === 'Screenplay' && screenplayFormat) {
+    selectedStory = `Selected Story: ${screenplayFormat} ${storyType}`;
+  } else {
+    selectedStory = `Selected Story: ${storyType}`;
+  }
 
+  // Display the selected story on the page
+  document.getElementById('selectedStory').innerHTML = `<h2>${selectedStory}</h2>`;
+}
 
-    {let savedStory = JSON.parse(localStorage.getItem('story')) || {};}
-    
+// Show the screenplay formats dropdown if the Screenplay option is selected
+document.getElementById('storyType').addEventListener('change', (event) => {
+  const screenplayFormatsDiv = document.querySelector('.screenplay-formats');
+  if (event.target.value === 'Screenplay') {
+    screenplayFormatsDiv.style.display = 'block';
+  } else {
+    screenplayFormatsDiv.style.display = 'none';
+  }
+});
 
-  })
+// Function to save the story to local storage
+function saveResults() {
+  const title = document.getElementById('title').value;
+  const storyGenre = document.getElementById('storyGenre').value;
+  const storyType = document.getElementById('storyType').value;
+  const screenplayFormat = document.getElementById('screenplayFormat')?.value;
+
+  const story = {
+    title,
+    storyGenre,
+    storyType,
+    screenplayFormat
+  };
+
+  localStorage.setItem('story', JSON.stringify(story));
+  alert('Story saved to local storage');
+}
+
+// Function to start over and clear the form and selected story
+function startOver() {
+  document.getElementById('add-story-form').reset();
+  document.getElementById('selectedStory').innerHTML = '';
+  localStorage.removeItem('story');
+  alert('Form and selected story cleared');
+}
+
